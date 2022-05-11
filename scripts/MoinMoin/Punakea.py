95bd799af443cdd0f970b94cfcac87e2b6c7d348
Sourcery AI <>
2022-05-11 09:47:06 +0000

    'Refactored by Sourcery'

--- scripts/MoinMoin/Punakea.py
+++ scripts/MoinMoin/Punakea.py
@@ -17,24 +17,24 @@ Dependencies = []
 def execute(macro, args):
 	nntagging = NNTagging()
 	tagNames = args.split(',')
-	tags = [nntagging.tagForName(tagName) for tagName in tagNames] 
+	tags = [nntagging.tagForName(tagName) for tagName in tagNames]
 	files = nntagging.executeQueryForTags(tags)
 	paths = [nnfile.path() for nnfile in files]
-	
+
 	out = '\n<div class="punakea"><b>Files for "' + ', '.join(tagNames) + '"</b>\n<br/ >\n<ul>\n'
-	
+
 	for path in paths:
 		name = path.split('/')[-1]
 		encodedpath = path.replace(' ','%20')
 		link = '<a href="file://' + encodedpath + '">' + name + '</a>'
-		out += '<li>' + link + '</li>\n'
-		
+		out += f'<li>{link}' + '</li>\n'
+
 	out += '</ul>\n'
-	
+
 	punakea_link = 'punakea://' + '/'.join(tagNames)
 	click_here = '<a href="' + punakea_link + '">Show "' + ', '.join(tagNames) + '" in Punakea</a>'
-	
+
 	out += '<br />\n' + click_here
 	out += '</div>\n'
-	
+
 	return out
